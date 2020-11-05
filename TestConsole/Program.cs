874c02748d22ca704f778ae3e66a5ba17ef1a5e7
Григорьев Василий Анатольевич <wwwvg@outlook.com>
2020-11-05 11:15:56 +0300

    Поправил комментарии

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -13,7 +13,7 @@ namespace TestConsole
         {
             Queen queen = new Queen();
             queen.SetProhibitedPositions(4,4);
-            queen.printProhibitedPos();
+            queen.PrintProhibitedPositions();
         }
     }
 
@@ -22,20 +22,21 @@ namespace TestConsole
         const int dim = 8;
         bool[,] prohibitedPositions = new bool[dim, dim];
 
-        public void SetProhibitedPositions(int ciY, int ciX)//current index
+        public void SetProhibitedPositions(int ciY, int ciX)//координаты расположения фигуры
         {
             for (int y = 0; y < dim; y++)//вертикаль
             {
                 for (int x = 0; x < dim; x++)//горизонталь
                 {
                     if (x == ciX && y >= ciY && y+1 < dim) prohibitedPositions[y+1, x] = true; //закрываются вертикальные позиции
-                    if (x == ciX && y >= ciY && x+1+y-ciY  < dim && y+1 <dim ) prohibitedPositions[y+1, x+1+y-ciY] = true; //положительная горизонталь
-                    if (x == ciX && y >= ciY && x-1-y+ciY >= 0 && y+1 < dim) prohibitedPositions[y+1, x-1-y+ciY] = true; //отрицательная горизонталь
+                    if (x == ciX && y >= ciY && x+1+y-ciY  < dim && y+1 <dim ) prohibitedPositions[y+1, x+1+y-ciY] = true; //закрывается положительная диагональ
+                    if (x == ciX && y >= ciY && x-1-y+ciY >= 0 && y+1 < dim) prohibitedPositions[y+1, x-1-y+ciY] = true; //закрывается отрицательная диагональ
+                    //горизонтальные позиции можно не закрывать т.к. при след. итерации фигура в эти позиции в любом случае не ставится
                 }
             }
         }
 
-        public void printProhibitedPos()
+        public void PrintProhibitedPositions()
         {
             for (int y = 0; y < dim; y++)
             {
